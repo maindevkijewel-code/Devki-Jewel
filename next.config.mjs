d7afd29540d60f7c1f79dad484c521ba852c7c06
Bedia Tanis Ghanshyambhai <tanisbedia@Bedias-MacBook-Air.local>
2026-05-08 20:05:26 +0530

    Fixed Vercel SSR issue

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,19 @@ const nextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+        stream: false,
+        encoding: false,
+      };
+    }
+    return config;
+  },
 }
 
 export default nextConfig
